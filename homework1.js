d4fcb8593e53edb9cec783f9977e7341b19c5b5d
Spartak777s <138505616+Spartak777s@users.noreply.github.com>
2023-07-14 17:05:08 +0400

    Add files via upload

--- homework1.js
+++ homework1.js
@@ -1,54 +1,95 @@
-// 1.Declare 2 variables a and b, such that a & gt; b, and define values for them.
-let res
-function calculator(a, b) {
-    let sum = a + b
-    let difference = a - b
-    let product = a * b
-    let quotient = a / b
-    let remainder = a % b
-    res =`Sum: ${sum}, difference: ${difference}; product:  ${product}; quotient: ${quotient}, remainder: ${remainder}`
-    return res
-}
-
-
-
-
-
-// Print your name and age in the following format: “My name is ____, I am ____
-function presentation(name,age){
-    return `My name is ${name}, I am ${age}`
-}
-
-
-
-
-// Declare a variable with defined value.Print the last digit of the number.
-function lastDig(num1) {
-    let res = num1.toString()
-    console.log(res)
-    return res.slice(-1)
-}
-
-
-// Check whether a given number is negative.Print “yes”, if it is negative, print “no”
-// otherwise.
-function checkingNeg(num2) {
-    if (num2 >= 0) {
-        console.log('no')
-    } else {
-        console.log('yes')
+// 1. Given an array of numbers.Write a function to separate odd and even numbers in
+// different arrays.
+function evenOdd(arr) {
+    let arrOdd = []
+    let arrEven = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            arrEven.push(arr[i])
+        } else {
+            arrOdd.push(arr[i])
+        }
     }
+    return `arrEven:${arrEven}, arrOdd:${arrOdd}`
 }
 
 
-// For a given three digit number calculate the sum of its digits.
-function digSum(num3) {
-    let res = num3
-    let sum = res.split('')
-    let result = 0
-    for (i = 0; i < sum.length; i++) {
-        result += +sum[i]
+// array1 = [12, 3, 6, 17, 0, 45]
 
+
+// Write a function that calculates sum, difference, multiplication and division between
+// given array elements depending on passed operation symbol.Write appropriate function
+// for each operation.
+function calculator(arr, symbol) {
+    let result = arr[0]
+    for (let i = 1; i < arr.length; ++i) {
+        if (symbol === '+') {
+            result += arr[i]
+        } else if (symbol === '-') {
+            result -= arr[i]
+        } else if (symbol === '*') {
+            result *= arr[i]
+        } else if (symbol === '/') {
+            result /= arr[i]
+        }
     }
     return result
+}
+
+// calculator(arr12, '+')
+// arr12=[2, 3, 4, 5]
+
+// Write a function which receives two strings and removes appearances of the second
+// string from the first one.
+function removesymb(str, exp) {
+    let removedStr = exp
+    let result = ''
+    for (let key in str) {
+        // console.log(removedStr)
+        if (!removedStr.includes(str[key])) {
+            result += (str[key])
+
+        }
+    }
+    return result
+}
+
+
+// 4. Given an object.Invert it(keys become values and values become keys).If there is
+// more than key for that given value create an array.
+
+
+function changeValueToKeyy(obj) {
+    const resultObj = {};
+    for (let key in obj) {
+        if (!(obj[key] in resultObj)) {
+            resultObj[obj[key]] = key;
+        } else {
+            let arr = [resultObj[obj[key]]];
+            arr.push(key);
+            resultObj[obj[key]] = arr
+        }
+    }
+    return resultObj
+}
+obj = { b: '1', c: '2', d: '2', g: '3', j: '3' }
+
+
+
+
+
+
+// 5. Given two objects.Write a function that performs shallow compare.
+function shallowcopare(obj1, obj2) {
+    for (let key in obj1) {
+        if (!(key in obj2) || obj1[key] !== obj2[key]) {
+            return false;
+        }
+    }
+    for (let key in obj2) {
+        if (!(key in obj1) || obj1[key] !== obj2[key]) {
+            return false;
+        }
+    }
+    return true;
 }
